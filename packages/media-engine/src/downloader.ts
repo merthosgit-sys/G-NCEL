68e8d017588af6a892d9c39f5271044adcbd8ca4
merthosgit-sys <merthosgit@gmail.com>
2026-09-17 16:45:41 +0300

    Update downloader.ts

--- packages/media-engine/src/downloader.ts
+++ packages/media-engine/src/downloader.ts
@@ -1,1 +1,44 @@
+import axios from "axios";
+import fs from "fs";
 
+
+export async function downloadVideo(
+ url:string,
+ output:string
+){
+
+ const response =
+ await axios.get(
+  url,
+  {
+   responseType:"stream"
+  }
+ );
+
+
+ const writer =
+ fs.createWriteStream(
+  output
+ );
+
+
+ response.data.pipe(writer);
+
+
+ return new Promise(
+  (resolve,reject)=>{
+
+   writer.on(
+    "finish",
+    resolve
+   );
+
+   writer.on(
+    "error",
+    reject
+   );
+
+  }
+ );
+
+}
