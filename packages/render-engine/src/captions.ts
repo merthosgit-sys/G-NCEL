8f72b72d33631a33047d8c25c2a0b2066e6302ed
merthosgit-sys <merthosgit@gmail.com>
2026-09-17 17:32:53 +0300

    Update captions.ts

--- packages/render-engine/src/captions.ts
+++ packages/render-engine/src/captions.ts
@@ -1,1 +1,31 @@
+import fs from "fs/promises";
 
+
+
+export async function createCaptionFile(
+ text:string,
+ output:string
+){
+
+
+const content = `
+1
+00:00:00,000 --> 00:00:05,000
+
+${text}
+
+`;
+
+
+
+await fs.writeFile(
+ output,
+ content,
+ "utf-8"
+);
+
+
+
+return output;
+
+}
