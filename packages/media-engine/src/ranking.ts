ccc52a2510e79a9d8757265c4c8eca4f63784433
merthosgit-sys <merthosgit@gmail.com>
2026-09-17 16:45:23 +0300

    Add rankVideos function to sort videos by score

--- packages/media-engine/src/ranking.ts
+++ packages/media-engine/src/ranking.ts
@@ -1,1 +1,25 @@
+export function rankVideos(
+ videos:any[]
+){
 
+ return videos.sort(
+  (a,b)=>{
+
+   const aScore =
+    (a.width / a.height)
+    +
+    a.duration;
+
+
+   const bScore =
+    (b.width / b.height)
+    +
+    b.duration;
+
+
+   return bScore-aScore;
+
+  }
+ );
+
+}
