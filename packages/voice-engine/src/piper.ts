eff843aea82a92170f841b50f6cd6fcb058ab835
merthosgit-sys <merthosgit@gmail.com>
2026-09-17 19:22:30 +0300

    Update piper.ts

--- packages/voice-engine/src/piper.ts
+++ packages/voice-engine/src/piper.ts
@@ -1,48 +1,73 @@
 import { execFile } from "child_process";
 import { promisify } from "util";
+import fs from "fs/promises";
+import path from "path";
 
 
 const exec =
-promisify(execFile);
+  promisify(execFile);
 
 
 
 export async function generateVoice(
- text:string,
- output:string
-){
+  text: string,
+  output: string
+) {
 
-console.log(
-"Generating Turkish voice..."
-);
-
-
-
-await exec(
-"python",
-[
-"-m",
-"piper",
-
-"--model",
-"voices/tr_TR-dfki-medium.onnx",
-
-"--output_file",
-output
-],
-{
-input:text
-}
-);
+  console.log(
+    "Generating Turkish voice..."
+  );
 
 
-
-console.log(
-`Voice created: ${output}`
-);
+  const tempDir =
+    "output/audio";
 
 
+  await fs.mkdir(
+    tempDir,
+    {
+      recursive: true
+    }
+  );
 
-return output;
+
+  const inputFile =
+    path.join(
+      tempDir,
+      "script.txt"
+    );
+
+
+  await fs.writeFile(
+    inputFile,
+    text,
+    "utf-8"
+  );
+
+
+  await exec(
+    "python",
+    [
+      "-m",
+      "piper",
+
+      "--model",
+      "voices/tr_TR-dfki-medium.onnx",
+
+      "--input_file",
+      inputFile,
+
+      "--output_file",
+      output
+    ]
+  );
+
+
+  console.log(
+    `Voice created: ${output}`
+  );
+
+
+  return output;
 
 }
