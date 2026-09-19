88a35f31d9baadc8311f355caccc6ae107cd53aa
merthosgit-sys <merthosgit@gmail.com>
2026-09-20 00:24:11 +0300

    Update ranking.ts

--- packages/media-engine/src/ranking.ts
+++ packages/media-engine/src/ranking.ts
@@ -1,27 +1,158 @@
+export interface RankedVideo {
+
+    id:number;
+
+    width:number;
+
+    height:number;
+
+    duration:number;
+
+    title:string;
+
+    url:string;
+
+    videoUrl:string;
+
+}
+
+
+
+
+
+
+
 export function rankVideo(
-videos:any[]
-){
 
-return videos
-.flatMap(
-(video:any)=>
-video.video_files.map(
-(file:any)=>({
+    videos:RankedVideo[]
 
-...file,
+):RankedVideo | undefined {
 
-score:
-(file.width < file.height ? 100 : 0)
-+
-(file.height ?? 0)
 
-})
-)
-)
-.sort(
-(a,b)=>
-b.score-a.score
-)
-[0];
+
+    return videos
+
+    .sort(
+
+        (
+
+            a,
+
+            b
+
+        ) => {
+
+
+            const scoreA =
+
+            calculateScore(a);
+
+
+
+            const scoreB =
+
+            calculateScore(b);
+
+
+
+            return scoreB - scoreA;
+
+
+        }
+
+    )[0];
+
+}
+
+
+
+
+
+
+
+
+function calculateScore(
+
+    video:RankedVideo
+
+):number{
+
+
+    let score = 0;
+
+
+
+
+    if(
+
+        video.width >=1920
+
+    ){
+
+        score +=30;
+
+    }
+
+    else if(
+
+        video.width >=1280
+
+    ){
+
+        score +=20;
+
+    }
+
+
+
+
+
+    if(
+
+        video.duration >=5 &&
+
+        video.duration <=30
+
+    ){
+
+        score +=25;
+
+    }
+
+
+
+
+
+    if(
+
+        video.height >
+
+        video.width
+
+    ){
+
+        score +=25;
+
+    }
+
+
+
+
+
+    if(
+
+        video.title
+
+    ){
+
+        score +=20;
+
+    }
+
+
+
+
+
+    return score;
 
 }
